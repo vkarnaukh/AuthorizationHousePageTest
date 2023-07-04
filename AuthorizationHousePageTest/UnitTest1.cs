9fee1e729d182ca3d8dd19a92d03254fcc85a59a
vkarnau <valeriia.karnaukh@gmail.com>
2023-07-04 18:25:46 +0200

    Add project files.

---
+++ AuthorizationHousePageTest/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium;
+
+namespace AuthorizationHousePageTest
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+        //private By _cookiesButton;
+        private readonly By _cookiesButton = By.XPath("//button[text()='W porządku']");
+
+        private readonly By _kontoButton = By.XPath("//div[@data-testid='account-info-logged-false']");
+        private readonly By _loginInputField = By.XPath("//input[@id='login[username]_id']");
+        private readonly By _passwordInputField = By.XPath("//input[@id='login[password]_id']");
+        private readonly By _submitButton = By.XPath("//button[@data-selen='login-submit']");
+
+        private const string _login = "valeriia.karnaukh@gmail.com";
+        private const string _password = "Password1";
+
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new OpenQA.Selenium.Chrome.ChromeDriver("C:\\Users\\vkarnau\\source\\repos\\AuthorizationHousePageTest");
+            driver.Navigate().GoToUrl("https://www.housebrand.com/pl/pl/");
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            //var konto = driver.FindElement(kontoButton);
+            //konto.Click();
+            var cookies = driver.FindElement(_cookiesButton);
+            cookies.Click();
+            var konto = driver.FindElement(_kontoButton);
+            konto.Click();
+            var login = driver.FindElement(_loginInputField);
+            login.SendKeys(_login);
+            var password = driver.FindElement(_passwordInputField);
+            password.SendKeys(_password);
+            var submitButton = driver.FindElement(_submitButton);
+            submitButton.Click();
+
+        }
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
